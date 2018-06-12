2da88890532b4a8305dbb0fea18082ad31080de8
Olivia Perryman <operryman@kinaxis.com>
2018-06-12 11:50:16 -0400

    try port 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); // Server
 
 //Express App
 const app = express();
-var port = process.env.PORT || 3000;
+var port = 80;//process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Example app listening on port ' + port + ' !'));
 
